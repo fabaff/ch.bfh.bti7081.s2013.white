ec13b89c6ec4539f9c2d7238f2ce2249cb8efd62
Patrick Kofmel <kofmp1@bfh.ch>
2013-05-11 14:45:11 +0200

    update

--- ch.bfh.bti7081.s2013.white/src/main/java/ch/bfh/bti7081/s2013/white/MhcPmsMainUI.java
+++ ch.bfh.bti7081.s2013.white/src/main/java/ch/bfh/bti7081/s2013/white/MhcPmsMainUI.java
@@ -11,22 +11,21 @@ import com.vaadin.ui.VerticalLayout;
  * The Application's "main" class
  */
 @SuppressWarnings("serial")
-public class MhcPmsMainUI extends UI
-{
+public class MhcPmsMainUI extends UI {
 
-    @Override
-    protected void init(VaadinRequest request) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
-        
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
-        });
-        layout.addComponent(button);
-    }
+	@Override
+	protected void init(VaadinRequest request) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
+
+		Button button = new Button("Click Me");
+		button.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				layout.addComponent(new Label("Thank you for clicking"));
+			}
+		});
+		layout.addComponent(button);
+	}
 
 }
