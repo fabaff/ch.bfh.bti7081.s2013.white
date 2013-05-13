c57b238fb05887bc1e7a0add21d70ebe6db4e553
Patrick Kofmel <kofmp1@bfh.ch>
2013-05-13 12:50:41 +0200

    task01.1 test commit

--- ch.bfh.bti7081.s2013.white/src/main/java/ch/bfh/ti/soed/white/mhc_pms/ui/MhcPmsMainUI.java
+++ ch.bfh.bti7081.s2013.white/src/main/java/ch/bfh/ti/soed/white/mhc_pms/ui/MhcPmsMainUI.java
@@ -3,6 +3,7 @@ package ch.bfh.ti.soed.white.mhc_pms.ui;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 /**
  * The Application's "main" class
@@ -12,6 +13,7 @@ public class MhcPmsMainUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
+		VerticalLayout vertLayout = new VerticalLayout();
 		this.setContent(new Label("MHC PMS"));
 		
 		
