5b82946c7483b82564990fd337e039a2aef169c3
Patrick Kofmel <kofmp1@bfh.ch>
2013-05-18 00:47:11 +0200

    update

--- ch.bfh.bti7081.s2013.white/src/main/java/ch/bfh/ti/soed/white/mhc_pms/ui/MhcPmsUI.java
+++ ch.bfh.bti7081.s2013.white/src/main/java/ch/bfh/ti/soed/white/mhc_pms/ui/MhcPmsUI.java
@@ -1,8 +1,9 @@
 package ch.bfh.ti.soed.white.mhc_pms.ui;
 
-
-
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.HorizontalSplitPanel;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
 
 /**
@@ -10,10 +11,36 @@ import com.vaadin.ui.UI;
  */
 @SuppressWarnings("serial")
 public class MhcPmsUI extends UI {
-	
+
 	@Override
 	protected void init(VaadinRequest request) {
-		this.setContent(new MainPanel());
+//		MhcPmsVerticalSplitPanel verticalPanel = new MhcPmsVerticalSplitPanel();
+//		Panel panel = new Panel();
+//		Navigator navigator = this.getNavigator();
+//		if (navigator != null) {
+//			navigator.addView("", verticalPanel);
+//		} else {
+//			navigator = new Navigator(this, panel);
+//		}
+//
+//		HorizontalSplitPanel horizontalPanel = new HorizontalSplitPanel();
+//		horizontalPanel.setFirstComponent(new MenuBarComponent(navigator));
+//		horizontalPanel.setSecondComponent(panel);
+//
+//		verticalPanel.setFirstComponent(new HomeTitleBarComponent());
+//		verticalPanel.setFirstComponent(new PatientTableComponent());
+//		navigator.addView(MenuBarComponent.ButtonEnum.HOME.toString(),
+//				verticalPanel);
+//		verticalPanel = new MhcPmsVerticalSplitPanel();
+//		verticalPanel.setFirstComponent(new TitleBarComponent());
+//		verticalPanel.setFirstComponent(new PatientInfoComponent());
+//		navigator.addView(MenuBarComponent.ButtonEnum.PATIENT_INFO.toString(),
+//				verticalPanel);
+//		this.setContent(horizontalPanel);
+
+		// setComponentAlignment(button, Alignment.MIDDLE_CENTER);
+		// Notification.show("Welcome to the Animal Farm");
+		// this.setContent(new MainPanel());
 	}
 
 }
