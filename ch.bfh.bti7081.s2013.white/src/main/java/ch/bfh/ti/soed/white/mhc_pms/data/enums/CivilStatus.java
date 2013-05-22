7e96604950226183b79dd62480b6f0d73ab4aeef
Fabian Affolter <mail@fabian-affolter.ch>
2013-05-22 14:43:01 +0200

    Enum for civil status

---
+++ ch.bfh.bti7081.s2013.white/src/main/java/ch/bfh/ti/soed/white/mhc_pms/data/enums/CivilStatus.java
@@ -0,0 +1,12 @@
+package ch.bfh.ti.soed.white.mhc_pms.data.enums;
+
+/**
+ * @author		Gruppe White, I2p, BFH Bern, <a href="https://github.com/fabaff/ch.bfh.bti7081.s2013.white">Contact</a>
+ * @version		0.0.2 
+ * @since		0.0.2
+ * 
+ * Civil status of the patient
+ */
+public enum CivilStatus {
+	ledig, verheiratet, verwitwet
+}
