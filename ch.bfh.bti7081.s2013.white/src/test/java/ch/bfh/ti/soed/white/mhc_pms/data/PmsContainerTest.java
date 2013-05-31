2660a2d25b316ea53307496f30aaedc84274621f
Patrick Kofmel <kofmp1@bfh.ch>
2013-05-31 14:20:55 +0200

    Class PmsContainerTest added

---
+++ ch.bfh.bti7081.s2013.white/src/test/java/ch/bfh/ti/soed/white/mhc_pms/data/PmsContainerTest.java
@@ -0,0 +1,64 @@
+package ch.bfh.ti.soed.white.mhc_pms.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class PmsContainerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	@Ignore
+	public final void testPmsContainer() {
+		PmsContainer container = new PmsContainer<>(PCase.class);
+		
+		assertTrue(true);
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCurrentPCaseId() {
+//		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetCurrentPCaseId() {
+//		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDecrementCurrentPCaseItemId() {
+//		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testIncrementCurrentPCaseItemId() {
+//		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddEntityE() {
+//		fail("Not yet implemented"); // TODO
+	}
+
+}
