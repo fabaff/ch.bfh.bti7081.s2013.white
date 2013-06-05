bec6ec3060ec48074d521d06960f566b4a02a356
Fabian Affolter <mail@fabian-affolter.ch>
2013-06-05 10:48:16 +0200

    Title bar vertical split reduced

--- ch.bfh.bti7081.s2013.white/src/main/java/ch/bfh/ti/soed/white/mhc_pms/ui/MainPanel.java
+++ ch.bfh.bti7081.s2013.white/src/main/java/ch/bfh/ti/soed/white/mhc_pms/ui/MainPanel.java
@@ -22,7 +22,7 @@ class MainPanel extends HorizontalSplitPanel implements ComponentChangeListener 
 
 	// Static vars for positioning
 	private static final long serialVersionUID = 6726671929546867989L;
-	private static final float VERTICAL_SPLIT_POS = 100.0f;
+	private static final float VERTICAL_SPLIT_POS = 90.0f;
 	private static final float HORIZONTAL_SPLIT_POS = 140.0f;
 
 	/*
